6368c1079a8713e9292ea3ae7868211766b21723
Manoranjan Sahoo <manonix@outlook.com>
2020-07-03 01:15:46 +0530

    Updates

---
+++ precache-manifest.4c6a65d01978cf0de8e150fe1bf89758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe7cc777d8790c9f7b721715fda50a7",
+    "url": "/superheros/index.html"
+  },
+  {
+    "revision": "05ea263e059e3815d8da",
+    "url": "/superheros/static/css/main.eccaa74f.chunk.css"
+  },
+  {
+    "revision": "e4b7301300b9a3047a0e",
+    "url": "/superheros/static/js/2.29db906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superheros/static/js/2.29db906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ea263e059e3815d8da",
+    "url": "/superheros/static/js/main.73dbca84.chunk.js"
+  },
+  {
+    "revision": "49140bbd7f4a149ff15f",
+    "url": "/superheros/static/js/runtime-main.1291103d.js"
+  }
+]);
